e8a7bbc1bec49066c04723470979ddb168ffc98e
Shrikanta Kalantri <42779746+shrikanta8@users.noreply.github.com>
2026-04-07 20:58:55 +0530

    Update and rename Find duplicate elements to Find duplicate elements.java

---
+++ Streams/Find duplicate elements.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.function.Function;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    List<Integer> list = Arrays.asList(2,4,1,4,1);
+	    
+	   // (Using Set)
+	    Set<Integer> seen = new HashSet<>();
+	    
+	    Set<Integer> result = list.stream()
+	                            .filter(e -> !seen.add(e))
+	                            .collect(Collectors.toSet());
+	             
+	             
+	             
+	           
+        List<Integer> duplicates = list.stream()
+                                .collect(Collectors.groupingBy(
+                                        Function.identity(),
+                                        Collectors.counting()
+                                ))
+                                .entrySet()
+                                .stream()
+                                .filter(e -> e.getValue() > 1)
+                                .map(Map.Entry::getKey)
+                                .collect(Collectors.toList());
+                                
+		System.out.println(duplicates);
+	}
+}
+
