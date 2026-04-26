5ccdf5c89b6e9cd2165cad80e3c732cc6eb80a96
Shrikanta Kalantri <42779746+shrikanta8@users.noreply.github.com>
2026-04-26 21:16:01 +0530

    Create 1) Two_Sum.java

---
+++ Day 4: Arrays Part-IV /1) Two_Sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int n=nums.length;
+
+        for(int i=0;i<n;i++){
+            int diff = target - nums[i];
+
+            if(map.containsKey(diff)){
+                return new int[]{i,map.get(diff)};
+            }
+
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+    }
+}
